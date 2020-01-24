1f1943e4aea5e72dd7c6d60832f6c82b94e1eb49
Jacklyn Hu <jih045@its-pch440-35.ucsd.edu>
2020-01-23 18:21:04 -0800

    first project

---
+++ FlixClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlixClone
+//
+//  Created by Jacklyn Hu on 1/23/20.
+//  Copyright © 2020 hwang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
